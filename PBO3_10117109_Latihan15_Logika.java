8ffa18e5bb646a48db13b649beee4d6f75e37617
Stefanus17 <43497735+Stefanus17@users.noreply.github.com>
2018-09-23 19:10:21 +0700

    Add files via upload

---
+++ PBO3_10117109_Latihan15_Logika.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo3_10117109_latihan15_logika;
+
+/**
+ *
+ * @author 
+ * NAMA  : Stefanus Saputra
+ * KELAS : IF 3
+ * NIM   : 10117109
+ */
+public class PBO3_10117109_Latihan15_Logika {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        boolean a = true;
+        boolean b = false;
+        System.out.println("a && b = " + (a && b));
+        System.out.println("a || b = " + (a || b));
+        System.out.println("!a = " + !a);
+        System.out.println("!(a && b) = " + !(a && b));
+    }
+    
+}
